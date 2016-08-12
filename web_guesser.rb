982c857a461abcda5accb3362eebf886d494ee63
Chase Dunagan <cdunagan@utexas.edu>
2016-08-11 19:25:46 -0600

    color added

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,24 +4,28 @@ require 'sinatra/reloader'
 NUMBER = rand(100)
 
 get '/' do
-  guess = params["guess"]
+  guess = params["guess"].to_i
   message = display_message(guess)
-  erb :index, :locals => {:number => NUMBER,
-                          :message => message}
+  erb :index, :locals => {:message => message,
+                          :color => @color}
 end
 
 
 def display_message(guess)
-  guess = guess.to_i
   if guess > (NUMBER + 5)
+    @color = "red"
     @message = "WAY too high!"
   elsif guess < (NUMBER - 5)
+    @color = "red"
     @message = "WAY too low!"
   elsif guess > NUMBER
+    @color = "yellow"
     @message = "Too high!"
   elsif guess < NUMBER
+    @color = "yellow"
     @message = "Too low!"
   else
+    @color = "green"
     @message = "CORRECT! The secret number was #{NUMBER}!"
   end
 end
